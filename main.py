a9ecec35324a6aa05900f95b4b5f8c01c4624b41
XJ_gout <XJ_gout@outlook.com>
2025-01-14 13:05:21 +0800

    添加了参数debug

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import choice
+import const
+import argparse
+
 
 def main():
+    parser = argparse.ArgumentParser(description="Command Minecraft Launcher")
+    parser.add_argument('-d', '--debug', action='store_true', help='启用调试模式')
+    args = parser.parse_args()
+
+    if args.debug:
+        const.DEBUG = True
+
     choice.first_init_choice()
     choice.main_menu()
 
